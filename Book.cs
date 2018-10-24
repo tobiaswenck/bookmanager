282bec45d52b91c044f739c5efc5bb3ba518639b
Tobias Wenck <37943025+tobiaswenck@users.noreply.github.com>
2018-10-24 16:54:29 +0200

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookManager
+{
+    class Book
+    {
+        public String title;
+        public String author;
+        public Int64 isbn;
+        public Int16 seitenzahl;
+        public String verlag;
+        public Double preis;
+        public Int16 erscheinungsjahr;
+        public String genre;
+        public String einband;
+        public Boolean kaufempfehlung;
+
+        public Book(String title, String author, Int64 isbn, Int16 seitenzahl, String verlag, Double preis, Int16 erscheinungsjahr, String genre, String einband, Boolean kaufempfehlung)
+        {
+            this.title = title;
+            this.author = author;
+            this.isbn = isbn;
+            this.seitenzahl = seitenzahl;
+            this.verlag = verlag;
+            this.preis = preis;
+            this.erscheinungsjahr = erscheinungsjahr;
+            this.genre = genre;
+            this.einband = einband;
+            this.kaufempfehlung = kaufempfehlung;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BookManager
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        Book HarryPotter = new Book("Harry Potter und der Stein der Weisen", "Joanne K. Rowling", 9789604533084, 335, "Carlsen", 15.99, 1997, "Fiction", "Gebunden", true);
+        public MainWindow()
+        {
+            InitializeComponent();
+            Title.Text = HarryPotter.title;  
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
